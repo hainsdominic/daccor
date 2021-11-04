bb640af743273c64a38e7c7ecbb99b37cf20b226
hainsdominic <dominic@hainstech.com>
2021-11-04 09:27:31 -0400

    feat(Housing contract): wrote contract and tests

--- truffle-config.js
+++ truffle-config.js
@@ -1,14 +1,24 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
-  contracts_build_directory: path.join(__dirname, "app/src/contracts"),
+  contracts_build_directory: path.join(__dirname, 'app/src/contracts'),
+  compilers: {
+    solc: {
+      version: '0.8.9',
+    },
+  },
   networks: {
-    develop: { // default with truffle unbox is 7545, but we can use develop to test changes, ex. truffle migrate --network develop
-      host: "127.0.0.1",
+    develop: {
+      // default with truffle unbox is 7545, but we can use develop to test changes, ex. truffle migrate --network develop
+      host: '127.0.0.1',
       port: 8545,
-      network_id: "*"
-    }
-  }
+      network_id: '*',
+    },
+  },
+  mocha: {
+    reporter: 'eth-gas-reporter',
+    // timeout: 100000
+  },
 };
